60be8ded63a51eb6c226219f0026eeca5ee21e50
MkRroe <57726747+MkRroe@users.noreply.github.com>
2023-09-25 17:24:57 -1000

    setup

---
+++ GitHub_Connect Test_General/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Connect_Test_General
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
